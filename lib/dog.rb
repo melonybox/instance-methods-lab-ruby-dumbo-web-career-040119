4357f175d8ff5ddbc5980f6e9c1ba889538f8180
Gino Desangles <blushingbox@yahoo.com>
2019-04-03 02:16:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,5 +4,16 @@ class Dog
   end
   
   def sit
+    puts "The Dog is sitting"
+  end
+end
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  
+  def walk
+    puts "The Person is walking"
   end
 end
